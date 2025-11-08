cd41ebe6d90d0ba9feb339a37e4a5aa87a7e5b77
Ashley Igonia <i.ashley0330@gmail.com>
2025-11-08 14:00:06 +0800

    feat: implement profile photo upload with preview and removal

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,6 +1,8 @@
 export const BASE_URL = "https://equitrack-backend-kh8q.onrender.com/api/v1.0";
+const CLOUDINARY_CLOUD_NAME = "doxx8vaou";
 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
     REGISTER: "/register",
+    UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
